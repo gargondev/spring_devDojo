5e6b257ee607d695085a5cfd1a1cbe971737c1e7
heldeveloper <heliezer.developer@gmail.com>
2021-08-19 23:05:20 -0300

    Criado Date Util para Log inserido dentro de UserController DateUtil Utilizando @Log4j2

--- learning_spring/src/main/java/com/spring/devdojo/learning_spring/controller/UserController.java
+++ learning_spring/src/main/java/com/spring/devdojo/learning_spring/controller/UserController.java
@@ -1,19 +1,33 @@
 package com.spring.devdojo.learning_spring.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.spring.devdojo.learning_spring.domain.User;
+import com.spring.devdojo.learning_spring.util.DateUtil;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
 @RestController
 @RequestMapping("list")
+@Log4j2
 public class UserController {
+
+    private DateUtil dateUtil;
+
+    public UserController(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
+    }
+
+  
 
     @GetMapping(path = "user")
     public List<User> list(){
+        log.info(dateUtil.formatDateTimeToDataBaseStyle(LocalDateTime.now()));
         return List.of(new User("Heliezer"), new User("Iara"), new User("Giovanna"));
     }
     
